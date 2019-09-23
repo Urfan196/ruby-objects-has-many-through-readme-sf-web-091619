932476b0943522b28b8c7f982017cdebf23e6df7
Urfan Guliyev <urfan196@yahoo.com>
2019-09-23 20:32:28 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,4 +14,19 @@ attr_accessor :name, :years
     @@all
   end
 
+  def new_meal
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def meals
+    new_meal.map {|meal| meal.meal}
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+     meal_a.tip <=> meal_b.tip
+   end
+   best_tipped_meal.customer
+  end
+
 end
